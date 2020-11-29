946bf5bfbc6578db66b55dd2a54aab5a967ddf2a
eeyu <evanyu134@gmail.com>
2020-11-28 20:46:58 -0600

    New balance class written, to be binned

--- bin/Balance/Balance.ino
+++ bin/Balance/Balance.ino
@@ -2,10 +2,11 @@
 
 RobotDog dog;
 
-#define start_LED_pin 14
+#define start_LED_pin 22
 
 Timer measurement_update_timer;
 float measurement_interval = 3.0/1000;
+Rot error_integrator = ROT_ZERO;
 
 void setup() {
     Serial.begin(9600);
@@ -32,12 +33,15 @@ void loop() {
         Rot IMU_orientation = dog.getBodyIMUOrientation_fG2B();
         Rot IMU_rot_velocity = dog.getBodyIMURotVelocity_fG2B();
        // Rot Kine_orientation = dog.getBodyKinematicOrientation_fF2B();
+        error_integrator += IMU_orientation;
       
         //Rot desired_orientation = Kine_orientation/3-IMU_orientation;
-        Rot desired_orientation = -IMU_orientation;
+        Rot desired_orientation = -(IMU_orientation - IMU_rot_velocity * 0.04 + error_integrator * 0.015);
         Point desired_position = Point(0, 0, dog.getStartingHeight());
         dog.moveBodyToOrientation(desired_orientation, TIME_INSTANT);
         dog.moveBodyToPositionFromCentroid(desired_position, Frame::GROUND, TIME_INSTANT);
+        
+        Serial.print("velocity: "); IMU_rot_velocity.print();
         Serial.print("Desired: "); desired_orientation.print();
         Serial.print("Measured: "); IMU_orientation.print();
         measurement_update_timer.reset();
