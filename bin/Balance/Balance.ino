24c570a3d2b58102534ead008a650a1caf0d1246
eeyu <evanyu134@gmail.com>
2020-11-28 20:49:44 -0600

    cleanup

--- bin/Balance/Balance.ino
+++ bin/Balance/Balance.ino
@@ -49,29 +49,3 @@ void loop() {
   
     dog.operate();
 }
-
-
-//double kp = 1;
-//double kd = 0.1;
-//double ki = 0.001;
-//
-//int i = 1;
-//
-//double perr[3] = {0,0,0};
-//double derr[3] = {0,0,0};
-//double ierr[3] = {0,0,0};
-//
-//void loop() {
-//  double dt = timer - millis();
-//  timer = millis();
-//  double err[3] = {-IMU_getAngY(), -IMU_getAngZ(), IMU_getAngX()};
-//  for (int i = 0; i < 3; i++) {
-//    derr[i] = (err[i] - perr[i])/dt;
-//    perr[i] = err[i];
-//    ierr[i] = ierr[i] - err[i]*dt; // Why is this -??
-//  }
-//  dog.moveToPose(kp*perr[0] + kd*derr[0] + ki*ierr[0], kp*perr[1] + kd*derr[1] + ki*ierr[1], kp*perr[2] + kd*derr[2] + ki*ierr[2], 0, 0, 0);  
-//  
-//  //dog.moveToPose(-k*IMU_getAngY(), -k*IMU_getAngZ(), -k*-IMU_getAngX(), 0, 0, 0);  
-//
-//}
